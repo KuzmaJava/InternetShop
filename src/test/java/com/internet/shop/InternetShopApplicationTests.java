43b9537d7aa3827d7e079a944eed2f9fb24072b9
Yevhenii Kuzmenko <ykuzmenko@atlassian.com>
2022-05-16 16:59:44 +0300

    Added project

---
+++ src/test/java/com/internet/shop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internet.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
